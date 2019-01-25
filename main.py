9f269489cccbdedf8478f12353cbc2eb7ddb3b43
Yukikamome316 <yukikamome316@gmail.com>
2019-01-26 00:16:37 +0900

    first visual studio code commit

--- main.py
+++ main.py
@@ -13,20 +13,20 @@ async def on_ready():
     print('------')
     await bot.change_presence(status=discord.Status.idle, activity=discord.Game(name=f'Prefix {prefix} |This bot is developed'))
 
-@bot.commands
+@bot.command()
 async def echo(ctx,text:str):
     await ctx.send(text)
 
-@bot.commands
+@bot.command()
 async def calc(ctx,formula:str):
     await ctx.send(eval(formula))
 
-@bot.commands
+@bot.command()
 async def kick(ctx,user:discord.user,text:str):
     await ctx.guild.kick(user,reason=text)
     await ctx.send(f'{user}をKickしました。理由:{reason}')
 
-@bot.commands
+@bot.command()
 async def ban(ctx,user:discord.user,text:str):
     await ctx.guild.ban(user,reason=text)
     await ctx.send(f'{user}をKickしました。理由:{reason}')
